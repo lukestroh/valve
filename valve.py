5e339c0595fa08c59f3641ba365c8a7313631982
lukestroh <66746219+lukestroh@users.noreply.github.com>
2022-04-01 15:01:54 -0700

    asdf

---
+++ valve.py
@@ -0,0 +1,16 @@
+"""
+valve.py
+
+Luke Strohbehn
+"""
+class Valve:
+    def __init__(self, min_angle, max_angle, curr_angle):
+        self.min_angle = min_angle
+        self.max_angle = max_angle
+        self.curr_angle = curr_angle
+
+    def open(self):
+
+    def close(self):
+
+    def move_to_angle(self, angle):
